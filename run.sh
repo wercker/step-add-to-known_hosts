7e7edbf406c5e554d468c84b0f398fd37ffce8e9
Benno van den Berg <bennovandenberg@gmail.com>
2013-08-23 14:32:18 +0200

    Add safeguards

--- run.sh
+++ run.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 set -e
 
+# make sure $HOME/.ssh exists
+if [ ! -d "$HOME/.ssh" ]; then
+  debug "$HOME/.ssh does not exists, creating it"
+  mkdir -p $HOME/.ssh
+fi
+
+known_hosts_path="$HOME/.ssh/known_hosts"
+
+if [ ! -f "$known_hosts_path" ]; then
+  debug "$known_hosts_path does not exists, touching it and chmod it to 600"
+  touch $known_hosts_path
+  chmod 600 $known_hosts_path
+fi
+
+if [ ! -w "$known_hosts_path" ]; then
+  fail "$known_hosts_path exists, but it not writeable"
+fi
+
 # validate <hostname> exists
 if [ ! -n "$WERCKER_ADD_TO_KNOWN_HOSTS_HOSTNAME" ]
 then
@@ -14,7 +32,7 @@ ssh_keyscan_result=`mktemp`
 ssh-keyscan $WERCKER_ADD_TO_KNOWN_HOSTS_HOSTNAME > ssh_keyscan_result
 
 if [ ! -n "$WERCKER_ADD_TO_KNOWN_HOSTS_FINGERPRINT" ] ; then
-  cat $ssh_keyscan_result > $HOME/.ssh/known_hosts
+  cat $ssh_keyscan_result >> $HOME/.ssh/known_hosts
   warn "Skipped checking public key with fingerprint, this setup is vulnerable to man in the middle attack"
   success "Successfully added host $WERCKER_ADD_TO_KNOWN_HOSTS_HOSTNAME to known_hosts"
 else
